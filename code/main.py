d41bace9eee4ee49946386db212483e9c96ef03a
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2023-05-31 22:17:24 +0200

    fact

--- code/main.py
+++ code/main.py
@@ -2,6 +2,14 @@
 
 import os
 
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
 if __name__ == '__main__':
     print('Hello World!')
     os.exit(0)

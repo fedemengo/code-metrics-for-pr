e35815490fe2a1dd6189af80cec657a190d6c506
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2023-05-31 22:27:44 +0200

    add code

--- code/main.py
+++ code/main.py
@@ -11,6 +11,14 @@ def factorial(n):
         return n * factorial(n-1)
 
 
+def dec2bin(n):
+    print('dec2bin({})'.format(n))
+    if n == 0:
+        return ''
+    else:
+        return dec2bin(n//2) + str(n % 2)
+
+
 if __name__ == '__main__':
     print('Hello World!')
     os.exit(0)
